74ea15519030fdd9a75c6fe56456c1cc4b343918
Vipin Singh <svipin174@gmail.com>
2022-03-27 14:09:14 +0530

    passing props using functional compoents

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -2,6 +2,7 @@ interface colorProps {
   color: string;
 }
 
+// Using this react dont know it it is a compoent
 export const Child = ({ color }: colorProps) => {
   return (
     <div>
@@ -9,3 +10,17 @@ export const Child = ({ color }: colorProps) => {
     </div>
   );
 };
+
+// this is better approach and have upside and benefits
+// this is have some additional properties, like `propType, contentType, display name etc.`
+// to get those just use `ChildAsFC.displayName`
+// FC stands for function compoenet
+export const ChildAsFC: React.FC<colorProps> = ({ color }) => {
+  return (
+    <div>
+      <h1>Hello! My colorid {color}</h1>
+    </div>
+  );
+};
+
+// Note thse are ether approach you can use any as per need.
